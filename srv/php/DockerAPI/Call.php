b5881509346b833fffda16d7c68cf2cf2ad49a73
thomas-is <thomas@0l.fi>
2021-02-04 08:55:44 +0100

    :construction: refacto

--- srv/php/DockerAPI/Call.php
+++ srv/php/DockerAPI/Call.php
@@ -7,12 +7,7 @@ class Call {
   protected $socket = '/var/run/docker.sock';
   protected $endpoint;
   protected $method = 'GET';
-  protected $payload;
-
-
-  public function __construct() {
-    $this->payload = new \stdClass;
-  }
+  protected $body;
 
   public function get($endpoint) {
     $this->endpoint = (string) $endpoint;
@@ -20,32 +15,36 @@ class Call {
     $this->_relay();
   }
 
+  public function post($endpoint, $body = "") {
+    $this->endpoint = (string) $endpoint;
+    $this->method   = "POST";
+    $this->body     = (string) $body;
+    $this->_relay();
+  }
+
   private function _relay() {
 
     $curl = curl_init();
 
-    $opts = array();
-    $opts[CURLOPT_URL             ] = "http://localhost/{$this->endpoint}";
-    $opts[CURLOPT_RETURNTRANSFER  ] = true;
-    $opts[CURLOPT_FOLLOWLOCATION  ] = true;
-    $opts[CURLOPT_HTTP_VERSION    ] = CURL_HTTP_VERSION_1_1;
-    $opts[CURLOPT_CUSTOMREQUEST   ] = $this->method;
-    $opts[CURLOPT_UNIX_SOCKET_PATH] = $this->socket;
+    curl_setopt($curl, CURLOPT_URL             , "http://localhost/{$this->endpoint}");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER  , true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION  , true);
+    curl_setopt($curl, CURLOPT_HTTP_VERSION    , CURL_HTTP_VERSION_1_1);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST   , $this->method);
+    curl_setopt($curl, CURLOPT_UNIX_SOCKET_PATH, $this->socket);
 
     switch( $this->method ) {
       case "POST":
       case "PUT":
-        $opts[CURLOPT_POSTFIELDS] = json_encode($this->payload);
-        $opts[CURLOPT_HTTPHEADER] = array( "Content-Type: application/json" );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array( "Content-Type: application/json" ));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $this->body);
         break;
       default:
     }
 
-    curl_setopt_array( $curl, $opts );
-
     $answer = curl_exec($curl);
     $infos  = curl_getinfo($curl);
-    $code = $infos['http_code'];
+    $code   = $infos['http_code'];
 
     curl_close($curl);
 
